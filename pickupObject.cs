118cf6c037e040640a688b12e4425b7d84edb975
Megan Davis <davisma24@vcu.edu>
2016-02-23 16:48:43 -0500

    Fix carrying to follow cursor rather than camera

--- pickupObject.cs
+++ pickupObject.cs
@@ -2,16 +2,35 @@
 using System.Collections;
 
 public class pickupObject : MonoBehaviour {
-    //GameObject mainCamera;
-    public Camera mainCamera;
-    GameObject carriedObject;
+
+    /*The lines that are commented as //K are the lines that can be uncommented for keyboard
+    implementation. Lines commented //C are lines for the object to be picked up based on location
+    of main camera.
+    
+    The purpose of this script is pickup objects when the middle falcon button is clicked, and then have it move
+    around the screen. This script may be attached to any object, however the pickUpAble script must be attached
+    to the object you wish to move.
+    */
+
+    //C public Camera mainCamera; 
+
+    public GameObject theCursor; //This can be whatever object you wish to cast the ray from
+    GameObject carriedObject; 
     GameObject theHaptic;
     SphereManipulator sphereScript;
     bool carryingObject;
+
+    /*
+        Ray Length - how far the ray should extend from the GameObject to check for a hit
+        Distance - how far you wish the object to 'float' from the GameObject
+        Smooth - This is used to prevent very jerky movement when moving the GameObject around
+    */
+    public float rayLength;
     public float distance;
     public float smooth;
-    // Use this for initialization
-    void Start () {
+
+	// Use this for initialization
+	void Start () {
         theHaptic = GameObject.Find("Falcon");
         sphereScript = theHaptic.GetComponent<SphereManipulator>();
     }
@@ -31,20 +50,20 @@ public class pickupObject : MonoBehaviour {
 
     void carry(GameObject o)
     {
-        //Rigidbody rd = o.GetComponent<Rigidbody>();
-        //rd.isKinematic = true;
-        o.transform.position = Vector3.Lerp(o.transform.position,mainCamera.transform.position + mainCamera.transform.forward * distance, Time.deltaTime * smooth);
+        // o.transform.position = Vector3.Lerp(o.transform.position,mainCamera.transform.position + mainCamera.transform.forward * distance, Time.deltaTime * smooth);
+        o.transform.position = Vector3.Lerp(o.transform.position, theCursor.transform.position + theCursor.transform.forward * distance, Time.deltaTime * smooth);
     }
 
     void pickup()
     {
-        if(sphereScript.button_pressed == 0)//if(Input.GetKeyDown(KeyCode.E))
+        if (sphereScript.button_pressed == 0)//if (Input.GetKeyDown(KeyCode.E))//if(sphereScript.button_pressed == 0)
         {
             int x = Screen.width / 2;
             int y = Screen.height / 2;
-            Ray ray = mainCamera.ScreenPointToRay(new Vector3(x, y));
+            //Ray ray = theCursor.
+            //Ray ray = mainCamera.ScreenPointToRay(new Vector3(x, y));
             RaycastHit hit;
-            if(Physics.Raycast(ray, out hit))
+            if(Physics.Raycast(theCursor.transform.position, theCursor.transform.forward, out hit, rayLength))//if(Physics.Raycast(ray, out hit))
             {
                 pickupAble p = hit.collider.GetComponent<pickupAble>();
                 if(p != null)
